0c97cabeaa47798cfac8c4329ab7b0f3376173e3
John Doe <johndoe@example.com>
2024-10-20 20:24:41 +0100

    updating my work

--- src/container/ProductDetails.jsx
+++ src/container/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectedproduct, removeSelectedproduct } from '../redux/action/productActioin';
 
@@ -32,23 +33,24 @@ const ProductDetails = () => {
       {Object.keys(product).length === 0 ?(
         <div>...loading</div>
       ) : (
-        <div>
-          <div>
+        <div className=' flex justify-center mt-12'>
+          <div className=' flex justify-between mt-6'>
             <div>AND</div>
-            <div className=''>
-              <div className=''>
-                <img src={image} alt="" />
+            <div className=' flex justify-between space-x-10'>
+              <div className=' border w-64'>
+                <img src={image} alt="bag/img" className='' />
               </div>
-              <div className=''>
-                <h1>{title}</h1>
-                <h2><a className=''>${price}</a></h2>
-                <h3>{category}</h3>
-                <p>{description}</p>
+              <div className=' border w-[22em] p-5 space-y-3'>
+                <h1 className='font-bold'>{title}</h1>
+                <h2><a className=' bg-teal-600 p-1 lukako text-2xl font-bold text-white'>${price}</a></h2>
+                <h3 className=' bg-slate-200 p-2 '>{category}</h3>
+                <p className='text-sm'>{description}</p>
                 <div className='' tabIndex="0">
                   <div className=''>
                     <i className='shop icon'></i>
                   </div>
-                  <div className=''>Add to Cart</div>
+                  <div className=' bg-red-600 p-1 lukako text-xl 
+                  font-bold text-white w-40 addtocart'><Link to="/cart">Add to Cart</Link></div>
                 </div>
               </div>
 
